2e28977d0d538e8a5cf136c85b2bdeaaf48ce15d
danil <d_davletov@internet.ru>
2025-01-30 14:45:46 +0100

    Setting the player

--- src/components/player/machine.ts
+++ src/components/player/machine.ts
@@ -1,56 +1,76 @@
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
 
 const machine = createMachine({
-    id: "Video",
-    initial: "mini",
-    states: {
-      mini: {
-        description: "The video as a small thumbnail",
+  id: "Video",
+  initial: "mini",
+  context: {
+    isLarge: false
+  },
+  states: {
+    mini: {
+      on: {
+        toggle: {
+          target: "full",
+        },
+        resize: {
+          actions: 'toggleSize'
+        },
+      }
+    },
+    full: {
+      entry: "playVideo",
+      exit: "pauseVideo",
+      initial: "playing",
+      states: {
+      playing: {
         on: {
-          toggle: {
-            target: "full",
+          "video.ended": {
+            target: "stopped"
           }
         }
       },
-      full: {
-        entry: "playVideo",
-        exit: "pauseVideo",
-        initial: "playing",
-        description: "Video playing fullscreen",
-        meta: {
-            size: {
-                height: 400,
-                width: 600,
+      stopped: {
+        after: {
+          "1000": {
+            target: "#Video.mini",
+              actions: []
             }
-        },
-        states: {
-          playing: {
-            on: {
-              "video.ended": {
-                target: "stopped"
-              }
-            }
-          },
-          stopped: {
-            after: {
-              "1000": {
-                target: "#Video.mini",
-                actions: []
-              }
-            }
-          }
-        },
-        on: {
-          toggle: {
-            target: "mini",
-            description: "User clicks video"
-          },
-          "key.escape": {
-            target: "mini"
           }
         }
+      },
+      on: {
+        toggle: {
+          target: "mini",
+        },
+        resize: {
+          actions: 'toggleSize'
+        },
+        "key.escape": {
+          target: "mini"
+        }
       }
-    }
-  });
+    }, 
+  }
+}, {
+  actions: {
+    toggleSize: assign(
+      (context) => {
+        const el = document.getElementById("reactPlayer")
+        
+        if (el) {
+          if (context.context.isLarge) {
+            el.style.width = "500px";
+            el.style.height = "350px";
+          } else {
+            el.style.width = "1000px";
+            el.style.height = "700px";
+          }
+        }
+  
+        return {isLarge: !context.context.isLarge}
+      }
+    ),
+  }
+});
 
 export default machine
